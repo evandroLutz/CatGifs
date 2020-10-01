990d0cdff1575e795f8f58d51275098c3a5f26bf
Andre Jarrell <mail@andrejarrell.com>
2020-10-01 23:06:21 +1000

    ready

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 import { cats } from './cats.js';
 
-let shuffle = array => {
-    return array.sort(() => Math.random() - 0.5);
-};
-
 document.addEventListener('DOMContentLoaded', yall({
     observeChanges: true
 }));
@@ -11,13 +7,12 @@ document.addEventListener('DOMContentLoaded', yall({
 new Vue({
     el: '#cats',
     data: {
-        cats: shuffle(cats),
-        loaded: 12
+        loaded: 12,
+        cats: _.shuffle(cats)
     },
     methods: {
         load() {
-            let amount = this.loaded + 12;
-            if (amount > this.cats.length) {
+            if (this.loaded + 12 > this.cats.length) {
                 this.loaded += this.cats.length - this.loaded;
             } else {
                 this.loaded += 12;
